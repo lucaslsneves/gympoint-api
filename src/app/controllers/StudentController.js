4110cec6225eca93e4f6395b0956e7bd0f757729
lucasluz99 <lucasluzthatsme@gmail.com>
2020-05-04 08:29:23 -0300

    Add Model and Controller of Student

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,7 @@
+import Student from '../models/Student';
+
+export default {
+  async store(req, res) {},
+
+  async update(req, res) {},
+};
---
+++ src/app/models/Student.js
@@ -0,0 +1,20 @@
+import { Model, DataTypes } from 'sequelize';
+
+class Student extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        age: DataTypes.INTEGER,
+        weight: DataTypes.FLOAT,
+        height: DataTypes.FLOAT,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Student;
