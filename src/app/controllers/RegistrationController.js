a093db6e35d020be7d4524c2dc579a11ac344848
lucasluz99 <lucasluzthatsme@gmail.com>
2020-05-05 18:15:06 -0300

    Add RegistrationController

---
+++ src/app/controllers/RegistrationController.js
@@ -0,0 +1,15 @@
+import * as Yup from 'yup';
+
+import Registration from '../models/Registration';
+import Plan from '../models/Plan';
+import Student from '../models/Student';
+
+export default {
+  async index(req, res) {},
+
+  async store(req, res) {},
+
+  async update(req, res) {},
+
+  async delete(req, res) {},
+};
