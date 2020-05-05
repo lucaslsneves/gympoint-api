f5bba45a7964b70a46a69be7497bc4e495afd808
lucasluz99 <lucasluzthatsme@gmail.com>
2020-05-05 15:04:13 -0300

    Add store method in PlanController

--- src/app/controllers/PlanController.js
+++ src/app/controllers/PlanController.js
@@ -1,3 +1,5 @@
+import * as Yup from 'yup';
+
 import Plan from '../models/Plan';
 
 export default {
@@ -18,7 +20,29 @@ export default {
     return res.json(plans);
   },
 
-  async store(req, res) {},
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().positive().integer().required(),
+      price: Yup.number().positive().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { title } = req.body;
+
+    const planExist = await Plan.findOne({ where: { title } });
+
+    if (planExist) {
+      return res.status(400).json({ error: 'This plan already exists' });
+    }
+
+    const plan = await Plan.create(req.body);
+
+    return res.json(plan);
+  },
 
   async update(req, res) {},
 
