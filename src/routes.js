d14ada7c3374ebd9449e6ede82f3b32a3a632597
lucasluz99 <lucasluzthatsme@gmail.com>
2020-05-05 18:53:23 -0300

    Add associate method in Registration model

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
+import RegistrationController from './app/controllers/RegistrationController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,5 +20,10 @@ routes.get('/plans', PlanController.index);
 routes.post('/plans', PlanController.store);
 routes.put('/plans/:id', PlanController.update);
 routes.delete('/plans/:id', PlanController.delete);
+
+routes.get('/regisrations', RegistrationController.index);
+routes.post('/regisrations', RegistrationController.store);
+routes.put('/regisrations/:id', RegistrationController.update);
+routes.delete('/regisrations/:id', RegistrationController.delete);
 
 export default routes;
